e564af0e92726b94a7b068af2d32389e5a7dbf27
Jeremy Voorhis <jvoorhis@Jeremy-Voorhiss-MacBook-Pro.local>
2010-03-30 09:06:46 -0700

    Removed MVK::Core.coercing

--- lib/mvk/core.rb
+++ lib/mvk/core.rb
@@ -5,19 +5,5 @@ require 'mvk/core/action'
 module MVK
   module Core
     CompilationContext = Struct.new(:module, :function, :builder)
-    
-    # Takes a list of pairs of [type, any_value] to a list of 
-    # values matching their given type.
-    def coercing(arguments)
-      yield arguments.map { |(type, expr)|
-        case type
-        when :float then Core::FloatingExpr(expr)
-        when :int then Core::IntExpr(expr)
-        else raise "Unknown type #{type}"
-        end
-      }
-    end
-    module_function :coercing
-
   end
 end
