943e26d5c8c51180843a7ef6319512bce6c861aa
Jeremy Voorhis <jvoorhis@Jeremy-Voorhiss-MacBook-Pro.local>
2010-04-13 09:05:26 -0700

    Distilled PortAudioCallback#compile_callback! even more. Using chainable LLVM::Builder syntax. Removed use of Array/#reduce for sequencing.

--- lib/mvk/portaudio_callback.rb
+++ lib/mvk/portaudio_callback.rb
@@ -50,31 +50,31 @@ module MVK
           "mvk_portaudio_callback",
           mod.types[:pa_stream_callback]
         ) do |func, input, output, frame_count, time_info, status_flags, user_data|
-          builder = LLVM::Builder.create
           entry   = func.basic_blocks.append("entry")
-          builder.position_at_end(entry)
+          builder = LLVM::Builder.create.position_at_end(entry)
+          context = Core::CompilationContext.new(mod, func, builder)
           
-          context     = Core::CompilationContext.new(mod, func, builder)
-          frame_count = Core::Int.data(frame_count)
           phase_ptr   = builder.struct_gep(user_data, 0)
           phase_addr  = Core::Int.data(builder.ptr2int(phase_ptr, LLVM::Int))
           phase       = Core::Int.data(builder.load(phase_ptr))
+          frame_count = Core::Int.data(frame_count)
           buffer      = Core::Int.data(builder.ptr2int(output, LLVM::Int))
           sample_size = Core::Int.const(4)
           frame_size  = sample_size * outs.size
           
-          [Core::Action.step(frame_count) { |frame|
-             time = (phase + frame).to_double / self.sample_rate
-             outs.map.with_index { |sig, channel|
-               expr         = sig.call(time)
-               sample_index = channel * sample_size + frame * frame_size
-               sample_addr  = buffer + sample_index
-               Core::Action.store(expr.to_float, sample_addr, LLVM::Float)
-             }.reduce(:seq)
-           },
-           Core::Action.store(phase + frame_count, phase_addr, LLVM::Int),
-           Core::Action.return(Core::Int.const(0))
-          ].reduce(:seq).compile(context)
+          Core::Action.step(frame_count) { |frame|
+            time = (phase + frame).to_double / self.sample_rate
+            outs.map.with_index { |sig, channel|
+              expr         = sig.call(time)
+              sample_index = channel * sample_size + frame * frame_size
+              sample_addr  = buffer + sample_index
+              Core::Action.store(expr.to_float, sample_addr, LLVM::Float)
+            }.reduce(:seq)
+          }.seq(
+            Core::Action.store(phase + frame_count, phase_addr, LLVM::Int)
+          ).seq(
+            Core::Action.return(Core::Int.const(0))
+          ).compile(context)
         end
       end
       
